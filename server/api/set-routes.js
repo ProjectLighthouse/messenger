f5d4e0721d739c6ee44815d6ee30e4eb6f79c8bb
João Gabriel Lima <joaogabriellima.eng@gmail.com>
2018-06-25 10:41:36 -0300

    update routes of conversation api

--- server/api/set-routes.js
+++ server/api/set-routes.js
@@ -12,14 +12,13 @@ module.exports = function (app) {
   app.get('/api/', ConversationApi.getStatus);
 
   app.get('/api/conversation/:sender', ConversationApi.getAllConversations);
-  app.get('/api/conversation/:sender/:recipient', ConversationApi.getConversationByRecipients);
-  app.post('/api/conversation/:sender/:recipient', ConversationApi.sendMessageToRecipients);
-  app.post('/api/conversation/:conversation/:sender', ConversationApi.sendMessageToAll);
+  app.get('/api/conversation/recipient/:recipient', ConversationApi.getConversationByRecipients);
+  app.post('/api/conversation/send/:sender', ConversationApi.sendMessageToAll);
 
-  app.delete('/api/conversation/:conversationID/members', ConversationApi.removeMembers);
-  app.get('/api/conversation/:conversationID/members', ConversationApi.getMembers);
-  app.post('/api/conversation/:conversationID/members', ConversationApi.addMembers);
-  app.put('/api/conversation/:conversationID/members', ConversationApi.updateMembers);
+  app.post('/api/conversation/:conversationId/members', ConversationApi.addMembers);
+  app.delete('/api/conversation/:conversationId/:referenceId/members', ConversationApi.removeMembers);
+  app.get('/api/conversation/:conversationId/:referenceId/members', ConversationApi.getMembers);
+  app.put('/api/conversation/:conversationId/:referenceId/members', ConversationApi.updateMembers);
 
   app.get('/api/conversation/:conversationID/delivered', ConversationApi.getDeliveredStatus);
   app.get('/api/conversation/:conversationID/:member/delivered', ConversationApi.getDeliveredStatusByMember);
