4117a53e1e22176c97c78e1688e5017fec8edf62
João Gabriel Lima <joaogabriellima.eng@gmail.com>
2018-06-25 10:40:04 -0300

    adding new control functions into conversation-api

--- server/api/api/conversation-api.js
+++ server/api/api/conversation-api.js
@@ -5,33 +5,61 @@ module.exports = {
     res.send(Date());
   },
   getAllConversations(req, res) {
-    res.send();
+    const { sender } = req.params;
+    Message.getAllConversations(sender).then((conversations) => {
+      return res.send(conversations);
+    });
   },
   getConversationByRecipients(req, res) {
-    res.send();
-  },
-  sendMessageToRecipients(req, res) {
-    res.send();
+    const { recipient } = req.params;
+    Message.getConversationByRecipients(recipient).then((conversations) => {
+      return res.send(conversations);
+    });
   },
   sendMessageToAll(req, res) {
-    res.send();
+    const { sender } = req.params;
+    const body = req.body;
+    Message.sendMessageToAll(body.reference, sender, body.message, body.members).then((conversation) => {
+      return res.send(conversation);
+    });
   },
   addMembers(req, res) {
-    res.send();
+    const { conversationId } = req.params;
+    const body = req.body;
+    Message.addMembers(conversationId, body.members).then((conversation) => {
+      return res.send(conversation);
+    });
   },
   getMembers(req, res) {
-    res.send();
+    const { conversationId, referenceId } = req.params;
+    Message.getMembers(conversationId, referenceId).then((conversation) => {
+      return res.send(conversation);
+    });
   },
   removeMembers(req, res) {
-    res.send();
+    const { conversationId, referenceId } = req.params;
+    const body = req.body;
+    Message.removeMembers(conversationId, referenceId, body.foreignKeyId).then((conversation) => {
+      return res.send(conversation);
+    });
   },
   updateMembers(req, res) {
-    res.send();
+    const { conversationId, referenceId } = req.params;
+    const body = req.body;
+    Message.updateMembers(conversationId, referenceId, body.member).then((conversation) => {
+      return res.send(conversation);
+    });
   },
   getDeliveredStatus(req, res) {
-    res.send();
+    const { id } = req.params;
+    Message.getDeliveredStatus(id).then((conversation) => {
+      return res.send(conversation);
+    });
   },
   getDeliveredStatusByMember(req, res) {
-    res.send();
+    const { id, foreignKeyId } = req.params;
+    Message.getDeliveredStatusByMember(id, foreignKeyId).then((conversation) => {
+      return res.send(conversation);
+    });
   },
 };
